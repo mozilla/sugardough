87c11719dc77c804bf9d502335bf84b1938a8956
Giorgos Logiotatidis <seadog@sealabs.net>
2014-10-28 19:11:28 +0200

    NewRelic support.

--- sugardough/sugardough/wsgi.py
+++ sugardough/sugardough/wsgi.py
@@ -13,8 +13,9 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sugardough.settings')
 from django.conf import settings
 from django.core.wsgi import get_wsgi_application
 
+import newrelic
+from decouple import config
 from whitenoise.django import DjangoWhiteNoise
-
 
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
@@ -22,3 +23,10 @@ application = DjangoWhiteNoise(application)
 # Add media files
 if settings.MEDIA_ROOT and settings.MEDIA_URL:
     application.add_files(settings.MEDIA_ROOT, prefix=settings.MEDIA_URL)
+
+# Add NewRelic
+newrelic_ini = config('NEW_RELIC_CONFIG_FILE', default='newrelic.ini')
+newrelic_license_key = config('NEW_RELIC_LICENSE_KEY', default=None)
+if newrelic_ini and newrelic_license_key:
+    newrelic.agent.initialize(newrelic_ini)
+    application = newrelic.agent.wsgi_application()(application)
