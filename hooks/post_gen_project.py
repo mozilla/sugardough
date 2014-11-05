eeac125cb7e6e15eab37604392ef72c1dc4d2ec1
Giorgos Logiotatidis <seadog@sealabs.net>
2014-11-05 15:44:54 +0200

    Post gen hook to generate dev .env file.

---
+++ hooks/post_gen_project.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from uuid import uuid4
+
+print 'Generating development .env file'
+
+variables = {
+    'DEBUG': 'True',
+    'ALLOWED_HOSTS': 'localhost, 127.0.0.1',
+    'SECRET_KEY': str(uuid4()),
+    }
+
+with open('.env', 'w') as environment:
+    for key, value in variables.items():
+        environment.write('{key}={value}\n'.format(key=key, value=value))
