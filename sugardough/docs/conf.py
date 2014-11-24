d196714d7633d413f68f2ebbd91f9363e46c96f9
Giorgos Logiotatidis <seadog@sealabs.net>
2014-11-24 14:54:39 +0200

    Use readthedocs theme for documentation.

--- sugardough/docs/conf.py
+++ sugardough/docs/conf.py
@@ -15,6 +15,13 @@
 import sys
 import os
 
+on_rtd = os.environ.get('READTHEDOCS') == 'True'
+if not on_rtd:
+    import sphinx_rtd_theme
+    html_theme = 'sphinx_rtd_theme'
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+
+
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
@@ -98,7 +105,7 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'default'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
