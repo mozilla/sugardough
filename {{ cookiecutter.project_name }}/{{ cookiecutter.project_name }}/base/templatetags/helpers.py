42df6cb0db08f87d98a23b0b09fb01a384345b9e
Giorgos Logiotatidis <seadog@sealabs.net>
2015-06-05 12:32:17 +0300

    Simple urlparams filter.

--- {{ cookiecutter.project_name }}/{{ cookiecutter.project_name }}/base/templatetags/helpers.py
+++ {{ cookiecutter.project_name }}/{{ cookiecutter.project_name }}/base/templatetags/helpers.py
@@ -1,9 +1,8 @@
 import datetime
-import urllib
 import urlparse
 
 from django_jinja import library
-from django.utils.encoding import smart_str
+from django.utils.http import urlencode
 
 
 @library.global_function
@@ -15,6 +14,7 @@ def thisyear():
 @library.filter
 def urlparams(url_, hash=None, **query):
     """Add a fragment and/or query paramaters to a URL.
+
     New query params will be appended to exising parameters, except duplicate
     names, which will be replaced.
     """
@@ -22,20 +22,11 @@ def urlparams(url_, hash=None, **query):
     fragment = hash if hash is not None else url.fragment
 
     # Use dict(parse_qsl) so we don't get lists of values.
-    q = url.query
-    query_dict = dict(urlparse.parse_qsl(smart_str(q))) if q else {}
-    query_dict.update((k, v) for k, v in query.items())
+    query_dict = dict(urlparse.parse_qsl(url.query))
+    query_dict.update(query)
 
-    query_string = _urlencode([(k, v) for k, v in query_dict.items()
-                               if v is not None])
+    query_string = urlencode(
+        [(k, v) for k, v in query_dict.items() if v is not None])
     new = urlparse.ParseResult(url.scheme, url.netloc, url.path, url.params,
                                query_string, fragment)
     return new.geturl()
-
-
-def _urlencode(items):
-    """A Unicode-safe URLencoder."""
-    try:
-        return urllib.urlencode(items)
-    except UnicodeEncodeError:
-        return urllib.urlencode([(k, smart_str(v)) for k, v in items])
