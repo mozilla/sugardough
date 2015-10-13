2f54046724b685ed703be4c9638195192cfcf375
tyler46 <mail.doctor46@gmail.com>
2015-10-13 21:55:46 +0300

    Add contribute.json to generated project.

--- {{ cookiecutter.project_name }}/{{ cookiecutter.project_name }}/base/tests.py
+++ {{ cookiecutter.project_name }}/{{ cookiecutter.project_name }}/base/tests.py
@@ -7,3 +7,11 @@ class HomeTests(TestCase):
     def test_base(self):
         response = self.client.get(reverse('home'))
         assert b'csrfmiddlewaretoken' in response.content
+
+
+class TestContribute(TestCase):
+
+    def test_contribute_json(self):
+        response = self.client.get('/contribute.json')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response['Content-Type'], 'application/json')
