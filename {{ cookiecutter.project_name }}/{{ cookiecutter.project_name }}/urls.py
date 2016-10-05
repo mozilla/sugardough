eb2566d3a9054b33b3ad4bc49ba353711b7354d4
Giorgos Logiotatidis <glogiotatidis@users.noreply.github.com>
2016-10-05 19:16:15 +0300

    Upgrade to Django 1.10.1 (#129)
    * Upgrade to Django 1.10.1
    
    * Change view to callable.
    
    * Upgrade django-jinja to 2.2.1
    
    * Upgrade to django-session-csrf==0.7.1

--- {{ cookiecutter.project_name }}/{{ cookiecutter.project_name }}/urls.py
+++ {{ cookiecutter.project_name }}/{{ cookiecutter.project_name }}/urls.py
@@ -1,14 +1,16 @@
 from django.conf import settings
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.views.static import serve as static_serve
 
 from {{ cookiecutter.project_name }}.base import views
+
 
 urlpatterns = [
     url(r'^$', views.home, name='home'),
     url(r'^admin/', include(admin.site.urls)),
 
     # contribute.json url
-    url(r'^(?P<path>contribute\.json)$', 'django.views.static.serve',
+    url(r'^(?P<path>contribute\.json)$', static_serve,
         {'document_root': settings.ROOT}),
 ]
