a490290e0c7fb5702d151cb7b0a3f6ec9237ba8f
Giorgos Logiotatidis <seadog@sealabs.net>
2017-03-08 23:37:47 +0200

    Update docker tests.

--- bin/test_sugardough_docker.sh
+++ bin/test_sugardough_docker.sh
@@ -15,25 +15,8 @@ cd $TDIR
 cookiecutter --no-input $OLDPWD
 cd sugardough
 
-PROJECT_NAME=`echo "jenkins${JOB_NAME}${BUILD_NUMBER}" | sed s/_//g`
-DC_CMD="docker-compose --project-name $PROJECT_NAME"
-
-$DC_CMD run -T web flake8 sugardough
-
-# Wait for database
-$DC_CMD up -d db
-while true;
-do
-    CMD="docker run --link ${PROJECT_NAME}_db_1:db multicloud/netcat -z db 5432"
-    if eval ${CMD};
-    then
-        break;
-    fi;
-    sleep 1s;
-done;
-
-# Run Tests
-$DC_CMD run -T web ./manage.py test
+docker-compose run -T web flake8 sugardough
+docker-compose run -T web bash -c 'urlwait && ./manage.py test'
 
 # Delete virtualenv
 rm -rf $TDIR $ENVDIR
